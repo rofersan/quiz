dc4c6e3b81bbea491628bd28834888cdb1dce3b9
rofersan <r_z10@hotmail.com>
2015-07-13 22:48:18 +0200

    commit 12 - validacion de datos de entrada

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -24,13 +24,19 @@ exports.index = function(req, res) {
 		};
 	}
 	models.Quiz.findAll(query).then(function (quizes) {
-		res.render('quizes/index.ejs', {quizes: quizes});
+		res.render('quizes/index.ejs', {
+			quizes: quizes,
+			errors: []
+		});
 	});
 };
 
 // GET /quizes/{uuid}
 exports.show = function(req, res) {
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {
+		quiz: req.quiz,
+		errors: []
+	});
 };
 
 // GET /quizes/{uuid}/answer
@@ -39,7 +45,11 @@ exports.answer = function(req, res) {
 	if (req.query.respuesta.trim() === req.quiz.respuesta) {
 		respuesta = 'Correcto';
 	}
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: respuesta});
+	res.render('quizes/answer', {
+		quiz: req.quiz, 
+		respuesta: respuesta,
+		errors: []
+	});
 };
 
 // GET /quizes/new
@@ -49,20 +59,31 @@ exports.new = function(req, res) {
 			pregunta: 'Pregunta',
 			respuesta: 'Respuesta'
 		});
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {
+		quiz: quiz,
+		errors: []
+	});
 };
 
 // GET /quizes/create
 exports.create = function(req, res) {
-	console.log('*****CREATE');
 	var quiz = models.Quiz.build(req.body.quiz);
-	console.log(quiz.pregunta);
-	console.log(quiz.respuesta);
-	quiz.save(
-		{
-			fields: ["pregunta", "respuesta"]	
-		}).then(function(){
-			console.log('*****SAVED');
-			res.redirect('/quizes');
-		});
+	quiz.validate().then(function(err){
+		if(err) {
+			res.render('quizes/new', {
+				quiz: quiz,
+				errors: err.errors
+			});
+		} else {
+			quiz.save(
+				{
+					fields: ["pregunta", "respuesta"]	
+				}).then(function() {
+					res.redirect('/quizes');
+				});
+		}
+	});
+
+
+	
 };
--- models/quiz.js
+++ models/quiz.js
@@ -3,7 +3,21 @@
 module.exports = function(sequelize, DataTypes) {
 	return sequelize.define('Quiz',
 		{
-			pregunta: DataTypes.STRING,
-			respuesta: DataTypes.STRING
+			pregunta: {
+				type: DataTypes.STRING,
+				validate: {
+					notEmpty: {
+						msg: '--> Falta pregunta'
+					}
+				}
+			},
+			respuesta: {
+				type: DataTypes.STRING,
+				validate: {
+					notEmpty: {
+						msg: '--> Falta respuesta'
+					}
+				}
+			}
 		});
 }
