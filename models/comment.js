f8fd63150289d82c581c4827fc52156e17d1d172
rofersan <r_z10@hotmail.com>
2015-07-15 22:40:11 +0200

    commit 15 - crear comentario

---
+++ models/comment.js
@@ -0,0 +1,15 @@
+// Definicion del modelo Comment
+
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('Comment',
+		{
+			texto: {
+				type: DataTypes.STRING,
+				validate: {
+					notEmpty: {
+						msg: '--> Falta Comentario'
+					}
+				}
+			}
+		});
+}
--- models/models.js
+++ models/models.js
@@ -29,8 +29,16 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
-// Exportar definicion de la tabla Quiz
+// Importar la definicion de la tabla Comment en comment.js
+var Comment = sequelize.import(path.join(__dirname, 'comment'));
+
+// Relacion entre tablas
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+// EXportar tablas
 exports.Quiz = Quiz;
+exports.Comment = Comment;
 
 // sequelize.sync() crea e inicializa la tabla de preguntas en DB
 sequelize.sync().then(function() {
