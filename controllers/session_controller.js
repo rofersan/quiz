cc596f7a36fbfbd447ee46e2d2797f7e54ca34f3
rofersan <r_z10@hotmail.com>
2015-07-17 18:27:34 +0200

    commit 17 - autorizacion

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+// Middleware de autorizacion de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next) {
+	if (req.session.user) {
+		next();
+	} else {
+		res.redirect('/login');
+	}
+};
+
 // GET /login
 exports.new = function(req, res) {
 	var errors = req.session.errors || {};
